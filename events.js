68eb1fe38a7e82ca917c0e648c2e3bf6b2bf22f7
jamesanthony <sunqijun1523@gmail.com>
2017-06-09 14:08:31 +0800

    Update events.js

--- events.js
+++ events.js
@@ -1,55 +1,55 @@
-// events.js
-let Events = function() {
+var Events = function() {
 
-	var events = events || {}
+  var events = events || {}
 
-	function on(type, listen, once) {
-		once = once || false
-		events[type] = events[type] || []
-		if (~~events[type].indexOf(listen)) {
-			events[type].push({
-				type: type,
-				listen: listen,
-				once: once
-			})
-		}
-		return this
-	}
+  function on(type, listen, once) {
+    once = once || false
+    events[type] = events[type] || []
+    if (~~events[type].indexOf(listen)) {
+      events[type].push({
+        type: type,
+        listen: listen,
+        once: once
+      })
+    }
+    return this
+  }
 
-	function off(type, listen) {
-		if (!events[type]) return
-		for (var i = 0; i < events[type].length; i++) {
-			if (events[type][i].listen = listen)
-				events[type].splice(i, 1)
-		}
-		return this
-	}
+  function off(type, listen) {
+    if (!events[type]) return
+    for (var i = 0; i < events[type].length; i++) {
+      if (events[type][i].listen = listen)
+        events[type].splice(i, 1)
+    }
+    return this
+  }
 
-	function once(type, listen) {
-		this.on(type, listen, true)
-	}
+  function once(type, listen) {
+    this.on(type, listen, true)
+  }
 
-	function emit(type) {
-		if (!events[type]) return
-		var args = [].slice.call(arguments, 1)
-		for (var i = 0, len = events[type].length; i < len; i++) {
-			events[type][i] && events[type][i].listen.apply(null, args)
-			if (events[type][i] && events[type][i].once)
-				delete events[type][i]
-		}
-		return this
-	}
+  function emit(type) {
+    if (!events[type]) return
+    var args = [].slice.call(arguments, 1)
+    for (var i = 0, len = events[type].length; i < len; i++) {
+      events[type][i] && events[type][i].listen.apply(null, args)
+      if (events[type][i] && events[type][i].once)
+        delete events[type][i]
+    }
+    return this
+  }
 
-	return {
-		on,
-		off,
-		once,
-		emit
-	}
+  return {
+    on,
+    off,
+    once,
+    emit
+  }
 
 }()
 
 var proto = Element.prototype
+
 proto.on = Events.on
 proto.off = Events.off
 proto.once = Events.once
